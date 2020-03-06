554067d890aa35172003dbaa6edf11acbb3d88d8
dhruv970331 <dhruveshmalhotra186@gmail.com>
2020-03-06 21:22:37 +0530

    added home page and sqlite.db for quick access and updated requirements no change in functionality

--- movie_metadata/urls.py
+++ movie_metadata/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views import View
+from django.shortcuts import render
 
+class HomeView(View):
+    def get(self,request):
+        return render(request,"home.html")
 urlpatterns = [
+    path("",HomeView.as_view(),name="home"),
     path('admin/', admin.site.urls),
     path("search/",include("searchapp.urls"))
 ]
